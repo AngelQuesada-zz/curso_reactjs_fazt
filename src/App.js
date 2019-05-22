c376ad0470283d1fe94afb02d334ccc106b55f66
Ángel Quesada <aquesada@grupotadel.com>
2019-05-22 17:18:09 +0200

    Finalizado el tercer (y último) vídeo
    - Se ha finalizado en vídeo 3, y con él, el curso

--- src/App.js
+++ src/App.js
@@ -1,81 +1,90 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {todos} from './todos'
 import './App.css';
 
+// data
+import { todos } from './todos.json';
+
+// subcomponents
+import TodoForm from './components/TodoForm';
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      todos:todos
+      todos
     }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
 
-  priority_color(priority) {
-    switch (priority) {
-      case "high":
-        return "danger"
-        break;
-      case "medium":
-        return "warning"
-        break;
-      case "low":
-        return "success"
-        break;
-    
-      default:
-        break;
-    }
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
   }
 
   render() {
-
-    const todos = this.state.todos.map((todo,i)=> {
+    const todos = this.state.todos.map((todo, i) => {
       return (
-        <div className="col-md-4">
+        <div className="col-md-4" key={i}>
           <div className="card mt-4">
-            <div className="card-header">
-            <h3>{todo.title}</h3>
-            <span className={"badge badge-pill badge-"+this.priority_color(todo.priority)+" ml-2"}>
-              {todo.priority}
-            </span>
+            <div className="card-title text-center">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
             </div>
             <div className="card-body">
-            <p>{todo.description}</p>
-            <p className="text-muted">{todo.responsible}</p>
+              {todo.description}
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
             </div>
           </div>
         </div>
       )
-    })
+    });
 
+    // RETURN THE COMPONENT
     return (
       <div className="App">
-      {/* Navegador */}
-          <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">
-              <img src={logo} className="App-logo" alt="logo" />
-            </a>
-            <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="collapsibleNavId">
-              <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">Tareas <span className="badge badge-pill badge-light ml-2">{this.state.todos.length}</span><span className="sr-only">(current)</span></a>
-                </li>
-              </ul>
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
             </div>
-          </nav>
-          <div className="container">
-            <div className="row mt-4">
-              {todos} 
+
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>
             </div>
           </div>
-
+        </div>
       </div>
     );
   }
